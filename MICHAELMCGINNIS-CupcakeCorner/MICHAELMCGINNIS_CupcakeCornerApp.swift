48313e609c82e117c56ca9bad8a1ea494eed3fca
Rukosa <mwmcginnis490@gmail.com>
2022-03-23 14:48:02 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-CupcakeCorner/MICHAELMCGINNIS_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_CupcakeCornerApp.swift
+//  MICHAELMCGINNIS-CupcakeCorner
+//
+//  Created by Michael Mcginnis on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
